b6aeb479206e71e3497832433b11f14520a7e0a9
root <root@DESKTOP-3HCTC1T>
2023-11-16 14:07:16 +0530

    Initial commit.

---
+++ src/try_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { try_backend } from "../../declarations/try_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await try_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
